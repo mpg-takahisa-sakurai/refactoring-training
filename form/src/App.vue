c1d58fdab14537d1b5bb1aff9e6aba6b82c65368
櫻井 貴央 <takahisa.sakurai@pc-12-361.local>
2019-07-09 16:07:21 +0900

    validationまで

--- form/src/App.vue
+++ form/src/App.vue
@@ -1,17 +1,151 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <form novalidate id="js-form" method="post" @submit.prevent="onSubmit">
+        <div class="field">
+          <label class="label">名前</label>
+          <div class="control">
+            <input v-model='name' class="input" type="text" name="name" required>
+            <p v-show="nameError" class="help is-danger">名前を入力してください</p>
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">年齢</label>
+          <div class="control">
+            <input v-model='age' class="input" type="number" min="0" max="130" name="age" required>
+            <p v-show="ageError" class="help is-danger">年齢を入力してください</p>
+          </div>
+        </div>
+
+
+        <div class="field">
+          <label class="label">性別</label>
+          <div class="control">
+            <label class="radio">
+              <input v-model='gender' type="radio" name="gender" value="男性" checked>
+              男性
+            </label>
+            <label class="radio">
+              <input v-model='gender' type="radio" name="gender" value="女性">
+              女性
+            </label>
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">職業</label>
+          <div class="control">
+            <div class="select">
+              <select v-model='job' name="job" id="js-job">
+                <option>会社員</option>
+                <option>自営業</option>
+                <option>公務員</option>
+                <option>学生</option>
+                <option>その他</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div v-show="job_other" class="field field-option" id="js-job-name">
+          <label class="label">職業名</label>
+          <div class="control">
+            <input v-model='jobName' class="input" type="text" name="job-name">
+            <p v-show="jobNameError" class="help is-danger">職業名を入力してください</p>
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">お問い合わせ内容</label>
+          <div class="control">
+            <textarea v-model='body' class="textarea" name="body" required></textarea>
+            <p v-show="bodyError" class="help is-danger">お問い合わせ内容を入力してください</p>
+          </div>
+        </div>
+
+        <div class="field">
+          <div class="control">
+            <label class="checkbox">
+              規約に同意する <input v-model='agreement' type="checkbox" name="agreement" required>
+              <p v-show="agreementError" class="help is-danger">規約に同意してください</p>
+            </label>
+          </div>
+        </div>
+
+
+        <div class="field">
+          <div class="control">
+            <button type="submit" class="button is-link">送信</button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      name: '',
+      nameError: false,
+      age: 0,
+      ageError: false,
+      gender: 0,
+      job: '',
+      jobName: '',
+      jobNameError: false,
+      body: '',
+      bodyError: false,
+      agreement: false
+    }
+  },
+  computed: {
+    job_other(){
+      return this.job==='その他'
+    }
+  },
+  methods: {
+    validateAll(){
+      this.nameError = !this.name
+      this.ageError = !this.age
+      this.jobNameError = !this.jobName
+      this.bodyError = !this.body
+    },
+    validate(){
+      this.validateAll()
+      !this.nameError &&
+      !this.ageError &&
+      (!this.job_other || !this.jobNameError) &&
+      !this.bodyError &&
+      this.agreement
+    },
+    onSubmit(){
+      if(this.validate()){
+
+      }else{
+        alert('入力エラーがあります')
+      }
+    }
+  },
+  watch: {
+    name() {
+      this.nameError = !this.name
+    },
+    age() {
+      this.ageError = !this.age
+    },
+    jobName() {
+      this.jobNameError = !this.jobName
+    },
+    body() {
+      this.bodyError = !this.body
+    }
   }
 }
 </script>
@@ -21,8 +155,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ form/src/components/FormInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="field">
+          <label class="label">名前</label>
+          <div class="control">
+            <input class="input" type="text" name="name" required>
+            <p class="help is-danger">名前を入力してください</p>
+          </div>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'FormInput',
+  data: ()=>{
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
